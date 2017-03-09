e4979372bbc38084ef18386c22439547b489ea5c
paulholmes13 <holmes771@btinternet.com>
2017-03-09 16:37:28 +0000

    Update Config
    Update Config

---
+++ config.js
@@ -0,0 +1,5 @@
+var config = {};                                                                                                                                                                                                               
+
+config.mysql = {hostname:'xxxx', username:'xxxx', password:'xxxx', database:'xxxx' };
+
+module.exports = config; 
